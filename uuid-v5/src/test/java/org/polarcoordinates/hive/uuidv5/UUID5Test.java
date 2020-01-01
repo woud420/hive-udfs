cdb1226d12c3aee84cbdc5b3e78c1144b229d336
Jean-Michel Bouchard <jim@polarcoordinates.org>
2020-01-01 11:07:52 -0500

    fixed indent

--- uuid-v5/src/test/java/org/polarcoordinates/hive/uuidv5/UUID5Test.java
+++ uuid-v5/src/test/java/org/polarcoordinates/hive/uuidv5/UUID5Test.java
@@ -14,7 +14,7 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspe
 import org.junit.Test;
 
 public class UUID5Test {
-	
+
     @Test (expected = HiveException.class)
     public void emptyNamespace() throws Exception {
         UUID5 uuid5 = new UUID5();
@@ -24,12 +24,12 @@ public class UUID5Test {
 
         ObjectInspector[] inspectors = new ObjectInspector[] {insp1, insp2};
         uuid5.initialize(inspectors);
-        
+
         DeferredObject namespace = mock(DeferredObject.class);
-		DeferredObject value = mock(DeferredObject.class);
+        DeferredObject value = mock(DeferredObject.class);
 
         when(namespace.get()).thenReturn("");
-		when(value.get()).thenReturn("B073FHWTPL");
+        when(value.get()).thenReturn("B073FHWTPL");
 
         DeferredObject[] vals = new DeferredObject[] {namespace, value};
 
@@ -38,20 +38,20 @@ public class UUID5Test {
     }
 
     @Test (expected = HiveException.class)
-	public void emptyValue() throws Exception {
-	    UUID5 uuid5 = new UUID5();
+    public void emptyValue() throws Exception {
+        UUID5 uuid5 = new UUID5();
 
         StringObjectInspector insp1 = mock(StringObjectInspector.class);
         StringObjectInspector insp2 = mock(StringObjectInspector.class);
 
         ObjectInspector[] inspectors = new ObjectInspector[] {insp1, insp2};
         uuid5.initialize(inspectors);
-        
+
         DeferredObject namespace = mock(DeferredObject.class);
-		DeferredObject value = mock(DeferredObject.class);
+        DeferredObject value = mock(DeferredObject.class);
 
         when(namespace.get()).thenReturn("Keepa");
-		when(value.get()).thenReturn("");
+        when(value.get()).thenReturn("");
 
         DeferredObject[] vals = new DeferredObject[] {namespace, value};
 
@@ -59,26 +59,26 @@ public class UUID5Test {
         uuid5.close();
     }
 
-	@Test
-	public void createUUID() throws Exception {
-	    UUID5 uuid5 = new UUID5();
+    @Test
+    public void createUUID() throws Exception {
+        UUID5 uuid5 = new UUID5();
 
         StringObjectInspector insp1 = PrimitiveObjectInspectorFactory.javaStringObjectInspector;
         StringObjectInspector insp2 = PrimitiveObjectInspectorFactory.javaStringObjectInspector;
 
         ObjectInspector[] inspectors = new ObjectInspector[] {insp1, insp2};
         uuid5.initialize(inspectors);
-        
+
         DeferredObject namespace = mock(DeferredObject.class);
-		DeferredObject value = mock(DeferredObject.class);
+        DeferredObject value = mock(DeferredObject.class);
 
         when(namespace.get()).thenReturn("keepa");
-		when(value.get()).thenReturn("B073FHWTPL");
+        when(value.get()).thenReturn("B073FHWTPL");
 
         DeferredObject[] vals = new DeferredObject[] {namespace, value};
 
         Object uv5 = uuid5.evaluate(vals);
 
-		assertEquals("2e071d92-f119-5700-9190-f0904de83e94", uv5.toString());
+        assertEquals("2e071d92-f119-5700-9190-f0904de83e94", uv5.toString());
     }   
 }
